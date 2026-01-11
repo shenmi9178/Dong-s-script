0e2d4737b8a88e74da261794aa39187c03ca87ec
shenmi9178 <shenminb@gmail.com>
2026-01-11 13:02:21 +0800

    Update and rename 99夜.lua.txt to 冬某99夜.lua

--- 冬某99夜.lua
+++ 冬某99夜.lua
@@ -2,9 +2,9 @@ local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/rel
 local Confirmed = false
 
 WindUI:Popup({
-    Title = "shenmi脚本V2",
+    Title = "DM脚本V2",
     IconThemed = true,
-    Content = "欢迎尊贵的用户" .. game.Players.LocalPlayer.Name .. "使用shenmi脚本 当前版本型号:V2",
+    Content = "欢迎尊贵的用户" .. game.Players.LocalPlayer.Name .. "使用DM脚本 当前版本型号:V2",
     Buttons = {
         {
             Title = "取消",
@@ -24,9 +24,9 @@ WindUI:Popup({
 })
 function createUI()
     local Window = WindUI:CreateWindow({
-        Title = "shenmiV2脚本",
+        Title = "DMV2脚本",
         Icon = "palette",
-    Author = "尊贵的"..game.Players.localPlayer.Name.."欢迎使用shenmiV2脚本", 
+    Author = "尊贵的"..game.Players.localPlayer.Name.."欢迎使用DM脚本", 
         Folder = "Premium",
         Size = UDim2.fromOffset(550, 320),
         Theme = "Violet",
@@ -46,7 +46,7 @@ function createUI()
     })
 
     Window:EditOpenButton({
-        Title = "shenmi脚本V2",
+        Title = "DM脚本V2",
         Icon = "crown",
         CornerRadius = UDim.new(0, 8),
         StrokeThickness = 3,
